bc5b897853d19f8690b819fdbc7d0c4e16f1ca41
Bruna Bazaluk M. V <bruna.videira@usp.br>
2020-10-22 22:04:19 +0000

    pista feita

--- main.c
+++ main.c
@@ -11,36 +11,58 @@ typedef struct C //ciclista
 
 typedef struct pista
 {
+	int d; //largura da pista
 	int q; //quantidade de ciclistas
-	int f**; //fila de ciclistas
-	int p**;
+	int lin;
+	int col; 
+	int** p;
 } pista;
 
-int montaPista(int d, int n, pista P)
+int montaPista(int d, int n, pista* P)
 {
-	
-	for (int i=0; i<10; i++){
-	P->p[i][] = (int *)malloc(10*sizeof(int *));
-		for (int j=0; j<d; j++){
-		P->p[i][j] = (int)malloc(sizeof(int));
-		} 
+	int i;
+	P->p = (int **)malloc(d*sizeof(int *));
+	for (i=0; i<d; i++){
+		P->p[i] = (int *)malloc(10*sizeof(int));
+		//for (int j=0; j<d; j++){
+		//P->p[i][j] = (int)malloc(sizeof(int));
+		//} 
 	}
-
-	for(int k=0;k<n;k++){
-		aux[k]=k;
+	printf("debug1");
+	int v[n];
+	printf("debug2;");
+	int index;
+    for (i = 0; i < d; i++) {
+ 		index = (rand() %  (n - 2));
+		int aux = v[i];
+		v[i]=v[index];
+		v[index]=aux;
 	}
-	if (aux[k] != 0)
-		P->p[][] = random()%5;
-	
-}
+	//int r = n/5;
+	for (i = 0; i < d; i++) {
+		P->p[i/5][i%5] = v[i];
+	}
+} 
 
 int main(int argc, char** argv)
 {
+	printf("%d ",argc);//,argv[2]);
+	pista* P = (pista *)malloc(sizeof(pista));
+	P->d = atoi(argv[1]); //tem q ver se eh ```d n``` ou ```n d```
+	P->q = atoi(argv[2]); 
+	
+	printf("debug0");
+	montaPista(P->d,P->q,P); 
+	for(int j=0;j<(P->d);j++){
+		for(int k=0;k<(10);k++){
+			printf("|%d|",P->p[j][k]);
+		}
+		printf("\n");
+	}
 
-	pista = argv[1]; //tem q ver se eh ```d n``` ou ```n d```
 }
 
-/*	for (int i=0; i<10; i++){
+/*	for (i=0; i<10; i++){
 		D[i][] = (int *)malloc(10*sizeof(int *));
 		for (int j=0; j<d; j++){
 			D[i][j] = (int)malloc(sizeof(int));
