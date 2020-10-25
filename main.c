e1a73c2000f09ba2ab31352d1008ff4fb481a5bc
Bruna Bazaluk M. V <bruna.videira@usp.br>
2020-10-25 22:29:52 +0000

    corrida "funciona"

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ typedef struct C //ciclista
 	int lin; //linha da pista
 	int col; // coluna da pista
  	int vol; //volta
+	int vivo;
 } C;
 
 typedef struct {
@@ -29,59 +30,96 @@ typedef struct {
 	int go[MAX_CICLISTAS];
 } ThreadHelp;
 
+int cpo; //ciclistas que passaram a origem
+int ciclistasVivos;
+int voltaAtual = 1;
 C* ciclistas;
 Pista* pista;
 ThreadHelp* th;
+pthread_mutex_t mutex;
 
 void exibePista(Pista* P) {
+	fprintf(stderr,"volta %d\n",voltaAtual);
+	pthread_mutex_lock(&mutex);
 	for(int j=0;j<10;j++){
 		for(int k=0;k<(P->d);k++){
 			if (P->p[j][k] == 0) {
-				printf("| ");
+				fprintf(stderr,"| ");
 			}
 			else {
-				printf("|%d",P->p[j][k]);
+				fprintf(stderr,"|%d",P->p[j][k]);
 			}
 		}
-		printf("|\n");
+		fprintf(stderr,"|\n");
 	}
+	pthread_mutex_unlock(&mutex);
+	fprintf(stderr,"---------------------\n");
 }
 
 // vetor de ciclistas 
 void* ciclista_thread(void* i) {
 	int id = *((int *) i);
 	C cic = ciclistas[id-1];
+	int proxLin;
+	int proxCol;
 	//printf("[Thread] Sou o ciclista %d e comecei a rodar e estou na pista (%d, %d), minha vel: %d\n", id, cic.lin, cic.col, cic.vel);
 
 	printf("[Thread] %d comecei\n", id);
 	while (1) {
-
-		if (pista->p[cic.lin][(pista->d + (cic.col-1)%pista->d)%pista->d] == 0) {
-			printf("[Thread] %d me mexi para (%d, %d)\n", id, cic.lin, (pista->d + (cic.col-1)%pista->d)%pista->d);
-			pista->p[cic.lin][cic.col] = 0;
-			pista->p[cic.lin][(pista->d + (cic.col-1)%pista->d)%pista->d] = id;
+		proxCol=cic.col;
+		proxLin=cic.lin;
+		pthread_mutex_lock(&mutex);
+		int quem=pista->p[cic.lin][(pista->d + (cic.col-1)%pista->d)%pista->d] ;//quem esta na posicao que eu quero ir
+		pthread_mutex_unlock(&mutex);
+		if (quem == 0) {
+			proxCol = (pista->d + (cic.col-1)%pista->d)%pista->d;
+			printf("[Thread] %d me mexi para (%d, %d)\n", id, cic.lin, proxCol);
 		}
 		else {
-			int id_de_quem_atrapalha = pista->p[cic.lin][(pista->d + (cic.col-1)%pista->d)%pista->d];
-			printf("[Thread] %d esperando %d terminar sua rodada\n", id, id_de_quem_atrapalha);
-			while(th->arrive[id_de_quem_atrapalha-1] == 0) continue;
+			printf("[Thread] %d esperando %d terminar sua rodada\n", id, quem);
+			while(th->arrive[quem-1] == 0) continue;
 			printf("[Thread] %d terminou a espera.\n", id);
-			if (pista->p[cic.lin][(pista->d + (cic.col-1)%pista->d)%pista->d] == 0) {
-				printf("[Thread] %d me mexi para (%d, %d)\n", id, cic.lin, (pista->d + (cic.col-1)%pista->d)%pista->d);
-				pista->p[cic.lin][cic.col] = 0;
-				pista->p[cic.lin][(pista->d + (cic.col-1)%pista->d)%pista->d] = id;
+			pthread_mutex_lock(&mutex);
+			int proxQuem = pista->p[cic.lin][(pista->d + (cic.col-1)%pista->d)%pista->d];
+			pthread_mutex_unlock(&mutex);
+			if (proxQuem == 0) {
+				proxCol = (pista->d + (cic.col-1)%pista->d)%pista->d;
+				printf("[Thread] %d me mexi para (%d, %d)\n", id, cic.lin, proxCol);
 			}
 			else {
 				printf("[Thread] %d tinha gente\n", id);
 			}
 		}
-
+		pthread_mutex_lock(&mutex);
+		pista->p[cic.lin][cic.col] = 0;
+		pista->p[proxLin][proxCol] = id;
+		pthread_mutex_unlock(&mutex);
+		cic.lin=proxLin;
+		cic.col=proxCol;
+		printf("id %d foi liberado\n", id);
 		th->arrive[id-1] = 1;
+		//OLHO NELE !!!
+		if(ciclistasVivos ==1) break;
+		
+		//ver se sou o ultimo e me destruir caso for
+		if(cic.col ==0 && voltaAtual%2==0){
+			//locka
+			cpo++;
+			if(cpo == ciclistasVivos){
+				//sou o ultimo
+				printf("[morre] %d volta %d\n",id,voltaAtual);
+				ciclistas[id-1].vivo = 0;
+				pista->p[cic.lin][cic.col] = 0;
+				ciclistasVivos--;
+				cpo=0;
+				break;
+			}
+		}
+		
 		while(th->go[id-1] == 0) continue;
 		th->go[id-1] = 0;
-
+		
 		printf("[Thread] %d acabei\n", id);
-		break;
 	}
 
 	// sortea a velocidade
@@ -95,8 +133,11 @@ void* ciclista_thread(void* i) {
 
 // runner -> loop
 void start_run(Pista* P){
+	exibePista(pista);
 	th = (ThreadHelp*) malloc(sizeof(ThreadHelp));
-	printf("[Main] Começando a corrida, uhuul!!\n");
+	int i = 0;
+
+	//printf("[Main] Começando a corrida, uhuul!!\n");
 	for (int i = 0; i < P->n; i++) {
 		th->ids[i] = i+1;
 		th->arrive[i] = 0;
@@ -104,10 +145,16 @@ void start_run(Pista* P){
 		//printf("[Main] Criei a thread %d\n", th->ids[i]);
 		pthread_create(&th->tids[i], NULL, ciclista_thread, (void *)&(th->ids[i]));
 	}
-
-	for (int rodada = 0; rodada < 1; rodada++) {
+	while (ciclistasVivos > 1) {
+		i++;
 		// Aqui eu espero todos os ciclistas rodarem
 		for (int cic_id = 0; cic_id < pista->n; cic_id++) {
+			printf("[zumbi] %d ->%d\n",cic_id+1,ciclistas[cic_id].vivo);
+			if(ciclistas[cic_id].vivo==0){ 
+				printf("%d nao sera executado\n", cic_id);
+				continue;
+			}
+			printf("%d esperando arrive\n",cic_id+1);
 			while (th->arrive[cic_id] == 0) continue;
 			printf("[Main] %d chegou no arrive\n", cic_id+1);
 		}
@@ -119,13 +166,17 @@ void start_run(Pista* P){
 		for (int cic_id = 0; cic_id < pista->n; cic_id++) {
 			th->go[cic_id] = 1;
 		}
+		exibePista(pista);
+		printf("**************************\n");
+		if(i%pista->d == 0)
+			//locka?
+			voltaAtual = (voltaAtual+1);
 	}
-
+	printf("FIM DO WHILE vivos-> %d\n",ciclistasVivos);
 	for (int i = 0; i < P->n; i++) {
 		pthread_join(th->tids[i], NULL);
 	}
-	printf("---------------------\n");
-	exibePista(pista);
+
 }
 
 void montaPista(int d, int n, Pista* P)
@@ -140,6 +191,7 @@ void montaPista(int d, int n, Pista* P)
 			P->p[i][j] = 0;
 		} 
 	}
+	ciclistasVivos = n;
 	int v[n];
 	for (i = 0; i < n; i++) {
 		v[i] = i + 1;
@@ -154,6 +206,7 @@ void montaPista(int d, int n, Pista* P)
 	}	
 	//int r = n/5;
 	for (i = 0; i < n; i++) {
+		ciclistas[i].vivo = 1;
 		P->p[i%5][i/5] = v[i];
 		// Olho nele!
 		ciclistas[v[i]-1].lin = i%5;
@@ -170,7 +223,9 @@ int main(int argc, char** argv)
 	pista->d = atoi(argv[1]); //tem q ver se eh ```d n``` ou ```n d```
 	pista->n = atoi(argv[2]); 
 	pista->n_vol = 0;
+	pthread_mutex_init(&mutex, NULL);
 	montaPista(pista->d,pista->n,pista);
-	exibePista(pista);
+	//exibePista(pista);
 	start_run(pista);
+	pthread_mutex_destroy(&mutex);
 }
